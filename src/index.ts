749ffb0bcec1c21f6b77d5fe77841689146f338e
Yesferal Cueva <yesferal@gmail.com>
2020-05-17 13:18:28 -0500

    Add Express App into index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello');
+import express from 'express';
+import { PORT } from './config/constants';
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Listening on Port: http://localhost:${PORT}`);
+});
