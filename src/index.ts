abe651815b09e8db87a4b81c5e2a7dac7686eeda
Yesferal Cueva <yesferal@gmail.com>
2023-11-15 10:30:44 -0500

    Implement rooms in Socket feature

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import mongoose from "mongoose";
+import express from 'express'
+import mongoose from "mongoose"
 import { concertRouter, bandRouter, venueRouter, stateRouter } from './routers'
 import { drawerController } from './controllers'
-import { Middleware }  from './middleware/middleware'
-import * as socketio from 'socket.io';
-import * as http from 'http';
+import { Middleware } from './middleware/middleware'
+import * as socketio from 'socket.io'
+import * as http from 'http'
 import * as path from "path"
 
 const PORT = process.env.PORT || ''
@@ -14,23 +14,23 @@ const SECRET = process.env.SECRET || ''
 var middleware = new Middleware(SECRET)
 
 mongoose
-.connect(DB_URL, { useNewUrlParser: true, dbName: 'hornsAppDB', useUnifiedTopology: true})
-        .then(() => console.log("Connected to MongoDB..."))
-        .catch(error => console.error(`Could not connect to MongoDB: ${error}`));
+    .connect(DB_URL, { useNewUrlParser: true, dbName: 'hornsAppDB', useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(error => console.error(`Could not connect to MongoDB: ${error}`))
 
-const app = express();
-app.use(express.json());
-app.use('/concert', middleware.verifyAuthorization, concertRouter);
-app.use('/band', middleware.verifyAuthorization, bandRouter);
-app.use('/venue', middleware.verifyAuthorization, venueRouter);
-app.use('/state', middleware.verifyAuthorization, stateRouter);
+const app = express()
+app.use(express.json())
+app.use('/concert', middleware.verifyAuthorization, concertRouter)
+app.use('/band', middleware.verifyAuthorization, bandRouter)
+app.use('/venue', middleware.verifyAuthorization, venueRouter)
+app.use('/state', middleware.verifyAuthorization, stateRouter)
 
 // Only to keep our free Heroku App alive
-app.get('/heroku', (req, res) => { return res.send('Hello, I am alive'); });
+app.get('/heroku', (req, res) => { return res.send('Hello, I am alive') })
 
 app.get("/", (req: any, res: any) => {
     res.sendFile(path.resolve("./src/index.html"))
-});
+})
 
 // Socket
 const server: http.Server = http.createServer(app)
@@ -38,12 +38,13 @@ const io: socketio.Server = new socketio.Server()
 io.attach(server)
 
 io.on('connection', async (socket: socketio.Socket) => {
-    
+
     console.log(`Connection : SocketId = ${socket.id}`)
 
     const versionCode = Number(socket.handshake.query.versionCode?.toString())
     const platform = socket.handshake.query.platform?.toString()
     if (versionCode && platform) {
+        socket.join(getSocketRoom(versionCode, platform))
         const drawer = await drawerController.findBy(versionCode, platform)
         if (drawer) {
             socket.emit('updateDrawer', JSON.stringify(drawer))
@@ -63,7 +64,9 @@ app.use('/updateDrawer', middleware.verifyAuthorization, async (req, res) => {
     if (versionCode && platform) {
         const drawer = await drawerController.findBy(versionCode, platform)
         if (drawer) {
-            io.emit('updateDrawer', JSON.stringify(drawer))
+            const room = getSocketRoom(versionCode, platform)
+            io.to(room).emit('updateDrawer', JSON.stringify(drawer))
+            console.log(`Send Updated-Drawer to Room ${getSocketRoom(versionCode, platform)}`)
 
             return res.status(200).json(drawer)
         }
@@ -72,6 +75,10 @@ app.use('/updateDrawer', middleware.verifyAuthorization, async (req, res) => {
     return res.status(400).json({ error: "No json drawer available" })
 })
 
+function getSocketRoom(versionCode: number, platform: string): string {
+    return versionCode + "+" + platform
+}
+
 server.listen(PORT, () => {
     console.log(`Http Server listening on Port: http://localhost:${PORT}`)
-});
+})
