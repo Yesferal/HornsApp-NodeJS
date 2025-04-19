e83933e7631636188a81dc7a0c173dabd738b7a1
Yesferal Cueva <yesferal@gmail.com>
2025-04-19 12:12:34 -0500

    Create title for ScreenRender model

--- src/models/render/screen.render.model.ts
+++ src/models/render/screen.render.model.ts
@@ -2,7 +2,7 @@
 
 import mongoose, { Schema, Document } from 'mongoose'
 import { ViewRenderScheme } from './view.render.model'
-import { DataRenderScheme } from './data.render.model'
+import { LocalizedStringSchema } from '../util/localized.string.model'
 
 export interface IScreenRender extends Document {
     views: IType[]
@@ -14,7 +14,7 @@ interface IType {
 
 export const ScreenRenderScheme: Schema = new Schema({
     key: { type: String },
-    data: { type: DataRenderScheme },
+    title: { type: LocalizedStringSchema },
     views: [{ type: ViewRenderScheme }],
 })
 
