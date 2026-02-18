cf23c019830b702ba684685944715723df7183e6
Yesferal Cueva <yesferal@gmail.com>
2026-02-18 06:26:19 -0500

    Replace document with InferShemaType in ScreenRender model

--- src/models/render/screen.render.model.ts
+++ src/models/render/screen.render.model.ts
@@ -1,16 +1,10 @@
 /* Copyright © 2025 HornsApp. All rights reserved. */
 
-import mongoose, { Schema, Document } from 'mongoose'
+import mongoose, { Schema, InferSchemaType } from 'mongoose'
 import { ViewRenderScheme } from './view.render.model'
 import { LocalizedStringSchema } from '../util/localized.string.model'
 
-export interface IScreenRender extends Document {
-    views: IType[]
-}
-
-interface IType {
-    key: string
-}
+export type IScreenRender = InferSchemaType<typeof ScreenRenderScheme>
 
 export const ScreenRenderScheme: Schema = new Schema({
     key: { type: String },
