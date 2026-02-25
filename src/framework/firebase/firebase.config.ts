7021fb800a4cbd1b3433cf223183f2ca7307edb8
Yesferal Cueva <yesferal@gmail.com>
2026-02-25 10:14:36 -0500

    Fix github action

--- src/framework/firebase/firebase.config.ts
+++ src/framework/firebase/firebase.config.ts
@@ -1,6 +1,11 @@
 // firebase-admin already includes its own TypeScript types.
 import admin from "firebase-admin"
-import serviceAccount from "../../../keys/muvin/serviceAccount.json"
+import { readFileSync } from "fs"
+
+const FIREBASE_KEY_PATH = process.env.FIREBASE_KEY_PATH || "../../../keys/muvin/serviceAccount.json"
+const serviceAccount = JSON.parse(
+  readFileSync(FIREBASE_KEY_PATH, "utf-8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
